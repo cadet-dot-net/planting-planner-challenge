fe270ab81c7e052824c2c900cf5a95c815de87cc
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2025-05-28 23:10:41 +0100

    create user accounts

---
+++ lib/planter_web/controllers/user_registration_html.ex
@@ -0,0 +1,5 @@
+defmodule PlanterWeb.UserRegistrationHTML do
+  use PlanterWeb, :html
+
+  embed_templates "user_registration_html/*"
+end
---
+++ lib/planter_web/controllers/user_settings_html.ex
@@ -0,0 +1,5 @@
+defmodule PlanterWeb.UserSettingsHTML do
+  use PlanterWeb, :html
+
+  embed_templates "user_settings_html/*"
+end
