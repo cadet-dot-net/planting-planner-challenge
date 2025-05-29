a6c757ba53bac8f6434b7fdcea8239fc99c1f8d1
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2025-05-29 03:48:00 +0100

    enable uploads in plan controller

--- lib/planter_web/controllers/plan_controller.ex
+++ lib/planter_web/controllers/plan_controller.ex
@@ -1,6 +1,8 @@
 defmodule PlanterWeb.PlanController do
   use PlanterWeb, :controller
 
+  alias Planter.Accounts.{Scope, User}
+  alias Util.CsvParser
   alias Plug.Conn
   alias Planter.Garden
   alias Planter.Garden.Plan
@@ -17,13 +19,16 @@ defmodule PlanterWeb.PlanController do
     render(conn, :index, plans: plans)
   end
 
-  def create(conn, %{"plan" => plan_params}) do
-    with {:ok, %Plan{} = plan} <- Garden.create_plan(conn.assigns.current_scope, plan_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/plans/#{plan}")
-      |> render(:show, plan: plan)
+  def create(conn, %{"file" => file}) do
+    %Plug.Upload{path: tmp_path} = file
+
+    case CsvParser.parse_rows(tmp_path, &parse_plan_row(&1, conn.assigns[:current_scope])) do
+      [] -> json(conn, %{"ok" => "Plan created"})
+      _ -> json(conn, %{"error" => "Failed to create plan"})
     end
+  rescue
+    reason ->
+      json(conn, %{"error" => "Failed to parse file: #{inspect(reason)}"})
   end
 
   def show(conn, %{"id" => id}) do
@@ -45,5 +50,22 @@ defmodule PlanterWeb.PlanController do
     with {:ok, %Plan{}} <- Garden.delete_plan(conn.assigns.current_scope, plan) do
       send_resp(conn, :no_content, "")
     end
+  end
+
+  defp parse_plan_row([_id, plant_name, area], scope) do
+    scope = scope || %Scope{user: %User{id: nil}}
+
+    %{soil_types: soil_types} = Planter.Knowledge.get_plant(plant_name)
+    area = String.to_float(area)
+    bed = Planter.Place.get_bed_by_soil_type_area(soil_types, area)
+    plant = Planter.Plant.get_vegetable!(plant_name)
+
+    plan_params = %{
+      area: area,
+      bed_id: bed.id,
+      plant_id: plant.id
+    }
+
+    Garden.create_plan(scope, plan_params)
   end
 end
